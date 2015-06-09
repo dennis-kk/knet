be69fd4633d4144ed9e2dcb53282d2431c6fb569
unknown <kuankuan.wang@wangkuankuan.mokun.com>
2015-06-09 18:18:46 +0800

    add version api

---
+++ knet/version.c
@@ -0,0 +1,29 @@
+#include "version.h"
+
+#define MAJOR 1 /* 主版本 */
+#define MINOR 3 /* 次版本 */
+#define PATCH 1 /* 补丁 */
+
+#define KNET_MAJOR(major) #major
+#define KNET_MINOR(minor) #minor
+#define KNET_PATCH(patch) #patch
+
+#define KNET_VERSION(major, minor, patch) \
+    KNET_MAJOR(major)"."KNET_MINOR(minor)"."KNET_PATCH(patch)
+
+const char* knet_get_version_string() {
+    static const char* version_string = KNET_VERSION(MAJOR, MINOR, PATCH);
+    return version_string;
+}
+
+int knet_get_version_major() {
+    return MAJOR;
+}
+
+int knet_get_version_minor() {
+    return MINOR;
+}
+
+int knet_get_version_path() {
+    return PATCH;
+}
